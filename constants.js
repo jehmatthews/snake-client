51981f9ef678eaf40ee9eaecd4fae94397b606b1
jehmatthews <jehmatthews30@gmail.com>
2022-09-05 16:08:37 -0700

    this constants file now holds const for ip and port as well as for every key movement and message logged through input.js where they are being exported to

---
+++ constants.js
@@ -0,0 +1,20 @@
+const IP = 'localhost';
+const PORT = 3000;
+const MOVE_UP = 'w';
+const MOVE_LEFT = 'a';
+const MOVE_DOWN = 's';
+const MOVE_RIGHT = 'd';
+const MESSAGES = {
+  'q': 'Say: come on!',
+  'e': 'Say: you messed up'
+};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  MOVE_RIGHT,
+  MESSAGES
+};
