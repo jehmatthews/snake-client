fc159adbb966096a1c4b7d2fb44d451cb361cc9c
jehmatthews <jehmatthews30@gmail.com>
2022-09-05 15:14:53 -0700

    logic as been updated from console.log's to connection.write so the connection is made to the server when called keys are being pressed and have also added 'let connection' in the global scope to be used throughout both functions. As well, connection var in setupInput as now been made to = 'conn'

--- input.js
+++ input.js
@@ -1,15 +1,34 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+  connection = conn;
   return stdin;
 };
 
 const handleUserInput = function (key) {
-  if (key === '\u0003') {
+  if (key === '\u0003') { // will quit the app
     process.exit();
+  }
+  // next logic moves the snake based on keys pressed
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left')
+  }
+  if (key === 's') {
+    connection.write('Move: down') 
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'q') {
+    connection.write('Say: come on!')
   }
 };
 
