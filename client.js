418ad80f1c281d71aee39e37758caa07de41bfcb
jehmatthews <jehmatthews30@gmail.com>
2022-09-05 13:35:32 -0700

    added some messages to be seen as soon as we make a connection to server, like succes and our name

--- client.js
+++ client.js
@@ -4,11 +4,18 @@ const connect = function () {
     host: "localhost",
     port: 3000,
   });
+
+  conn.on('connect', (data) => {
+    console.log("Succesfully connected to game server");
+  });
+
+  conn.on('connect', (data) => {
+    console.log("Name: JHM");
+  });
   
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
-  
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
